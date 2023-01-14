f8768aefce7036eda0d786630ef5a0438ba42e0f
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-01-14 17:14:12 +0300

    added an components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
 import searchicon from "./search-3-16-removebg-preview.png"
+
 function Headers(){
   return(
     <div className="heading">
@@ -11,22 +12,110 @@ function Headers(){
     </div>
   )
 }
-function Search({searchitem}){
+
+function Search({searchitem, handleSearchitem}){
   return(
     <div className='searchbar'>
-      <input name="search" value={searchitem}type="text " placeholder='Search Your Recent Transaction' />
+      <input  onInput={handleSearchitem} name="search" value={searchitem}type="text " placeholder='Search Your Recent Transaction' />
       <img src={searchicon} alt="search icon"/>
     </div>
   )
 }
 
+function  Table ({tableDetails}){
+
+
+  return (
+    <div className='table'>
+      <table>
+      <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th> Amount</th>
+          </tr>
+      </thead>
+      <tbody>
+        {
+          tableDetails.map((details)=>{
+            return(
+              <tr key={details.id}>
+                <td >{details.date}</td>
+                <td >{details.description}</td>
+                <td >{details.category}</td>
+                <td >{details.amount}</td>
+              </tr> 
+            )
+          })
+          }
+      </tbody>
+      </table>
+    
+    </div>
+
+  )
+  }
+
+  function Category({addTranscation}){
+    function submit (event){
+      event.preventDefault()
+      
+      const addedData ={
+        date: event.target.date.value,
+        description: event.target.description.value,
+        category:event.target.category.value,
+        amount:event.target.amount.value
+      }
+
+      alert(addedData)
+      addTranscation(addedData)
+    }
+    return(
+      <form className='formts' onSubmit={submit}>
+      <div className="inputs">
+        <label>Date:</label>
+        <input name="date" type="date" />
+        <input  name="description" placeholder='Description' type="text"/>
+        <input  name="category" placeholder='category' type="text"/>
+        <input  name="amount" placeholder='Amount' type="number"/>
+      </div>
+        <button className='trans'>Add Transaction</button>
+      </form>
+    )
+  }
+
 
 function App() {
-  const [ searchitem , setsearchitem]= useState()
+  const [ searchitem , setsearchitem]= useState("")
+  const [ tableDetails , settableDetails]= useState([])
+
+  function handleSearchitem(event){
+      setsearchitem(event.target.value)
+      
+  }
+  function fetchData(url){
+    fetch(url)
+    .then(resp=>resp.json())
+    .then(data=>{ settableDetails(data)})
+  }
+
+  function addTranscation(trans){
+    trans.id=tableDetails.length
+    settableDetails([...tableDetails, trans])
+  }
+
+  
+   useEffect(()=>{
+    fetchData("http://localhost:4000/transactions")
+   },[])
+  console.log(tableDetails)
   return (
     <div className='app'>
         <Headers/>
-        <Search searchitem={searchitem}/>
+        <Search handleSearchitem={handleSearchitem}searchitem={searchitem}/>
+        <Category addTranscation={addTranscation} />
+        <Table tableDetails={tableDetails}/>
     </div>
     
   )
